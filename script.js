4362f57b79f22712b5e613796a6d4efe0298002b
Luis Rujana <lmrujanass@gmail.com>
2020-11-04 22:04:06 -0500

    Added api call function

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-$(document).ready(function(){
-    $('#cities-btn').on('click', function(){
-        var newCity = $('<button>');
-        newCity.text($('#city-input').val());
-        newCity.addClass('list-group-item list-group-item-action');
-        $('#cities').prepend(newCity);
+//This code creates new city buttons
+$('#cities-btn').on('click', function () {
+    var newCity = $('<button>');
+    var cityInfo = $('#city-input').val().trim();
+    newCity.text(cityInfo);
+    newCity.attr('data-city', cityInfo);
+    newCity.attr('type', 'button');
+    newCity.addClass('list-group-item list-group-item-action');
+    newCity.addClass('city-btn');
+    $('#cities').prepend(newCity);
+    $('#city-input').val('');
+    console.log($('.city-btn'));
+});
+
+function weatherCall(cityName) {
+    var apiKey = '0104602cb76c0eb747c5952160d01b86';
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+    }).then(function (response) {
+        return response;
     })
-})
+};
+
+$('.city-btn').on('click', () => {
+    alert('Hello'); 
+});
